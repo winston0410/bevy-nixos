48c89c086a0d56453c3906fafddcd0995320203b
kghugo <hugosum.dev@protonmail.com>
2022-02-03 22:23:05 +0000

    Try and add more deps

--- flake.nix
+++ flake.nix
@@ -10,19 +10,34 @@
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk {};
+        naersk-lib = pkgs.callPackage naersk { };
       in {
 
         defaultPackage = naersk-lib.buildPackage ./.;
 
-        defaultApp = utils.lib.mkApp {
-            drv = self.defaultPackage."${system}";
-        };
+        defaultApp = utils.lib.mkApp { drv = self.defaultPackage."${system}"; };
 
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy pkg-config alsa-lib libudev-zero];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [
+              cargo
+              rustc
+              rustfmt
+              pre-commit
+              rustPackages.clippy
+              pkg-config
+              alsa-lib
+              libudev-zero
+              #NOTE Add more deps
+              vulkan-loader
+              xorg.libX11
+              x11
+              xorg.libXrandr
+              xorg.libXcursor
+              xorg.libXi
+            ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
 
       });
 
